92efea832e72f250f9d83b4b431ee92b64077d00
danondanissimo <danilievdanilo@gmail.com>
2022-10-02 18:49:40 +0300

    Adds tasks + solution to task 1

---
+++ js/task-01.js
@@ -0,0 +1,8 @@
+const categoriesList = document.querySelector('#categories');
+const amount = categoriesList.children;
+console.log(`Number of categories:${amount.length}`);
+
+[...amount].forEach(item => {
+    console.log(`Category:${item.querySelector('h2').textContent}`);
+    console.log(`Elements:${item.querySelectorAll('li').length}`);
+})
