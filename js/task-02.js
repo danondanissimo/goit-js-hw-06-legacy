799cab983c1b1b5b116dbcd981102831663abd83
danondanissimo <danilievdanilo@gmail.com>
2022-10-02 19:32:41 +0300

    Adds task 2 solution

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,23 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsBunch = document.querySelector('#ingredients');
+
+
+
+
+const ingredientsList = ingredients => {
+  return ingredients.map(ingredient => {
+    const item = document.createElement('li');
+    item.textContent = ingredient;
+    return item;
+
+  });
+};
+      
+const elementsList = ingredientsList(ingredients);
+
+  ingredientsBunch.append(...elementsList);
+
+console.log(ingredientsBunch);
