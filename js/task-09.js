81c06610836da52212fa299ec98e47f5d97fd6a4
danondanissimo <danilievdanilo@gmail.com>
2022-10-04 20:20:41 +0300

    Adds solution for task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const body = document.querySelector("body");
+const btnChangeColor = document.querySelector(".change-color");
+const span = document.querySelector(".color");
+
+btnChangeColor.addEventListener("click", (event) => {
+  body.style.backgroundColor = getRandomHexColor();
+  span.textContent = body.style.backgroundColor;
+});
