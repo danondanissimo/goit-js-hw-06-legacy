450670f58d5499a73dd03b394f2cbed0be37aaf8
danondanissimo <danilievdanilo@gmail.com>
2022-10-04 19:41:31 +0300

    Adds solution for task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const form = document.querySelector('.login-form');
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const formEls = event.currentTarget.elements;
+
+    const email = formEls.email.value;
+    const password = formEls.password.value;
+    const loginData = {email, password};
+    
+    if (email === "" || password === "") {
+        alert('Поля должны быть заполнены!')
+    } else {
+        console.log(loginData);
+        event.currentTarget.reset();
+    };
+  
+});
 
