67f61a85d6b65458afb068fa33ce2649bc8bb518
danondanissimo <danilievdanilo@gmail.com>
2022-10-06 11:23:22 +0300

    Adds fix for task 5

--- js/task-05.js
+++ js/task-05.js
@@ -1,9 +1,12 @@
 const input = document.querySelector('#name-input');
 const output = document.querySelector('#name-output');
 
-input.addEventListener('input', () => {
-    if (input.value === '') {
-        output.textContent = 'Anonymous'
+input.addEventListener('input', (event) => {    
+    if (event.currentTarget.value === "") {
+      output.textContent = "Anonymous"
+    } else {
+      output.textContent = event.currentTarget.value
     }
-    output.textContent = input.value;
-})
+  })
+
+
